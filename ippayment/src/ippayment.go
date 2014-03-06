fb9b54265fead2028bef2e4b2e20e944b19e4ef9
Sinelga <support@sinelga.com>
2014-03-07 01:25:53 +0200

    dev

--- ippayment/src/ippayment.go
+++ ippayment/src/ippayment.go
@@ -4,20 +4,23 @@ import (
     "net/http"
     "fmt"
     "jsonresponse"
+//    "net/url"
 )
 
 // Default Request Handler
 func defaultHandler(w http.ResponseWriter, r *http.Request) {
 //    fmt.Fprintf(w, "<h1>Hello %s!</h1>", r.URL.Path[1:])
 	msisdn := r.Header.Get("X-UP-CALLING-LINE-ID")
-//    fmt.Fprintf(w, r.Header.Get("X-UP-CALLING-LINE-ID"))
+	callback := r.URL.Query().Get("callback")
+
     r.Header.Set("Content-Type", "application/json")
-    fmt.Fprint(w, jsonresponse.Response{"success": true, "msisdn": msisdn})
+    jsonstr := jsonresponse.Response{"success": true, "msisdn": msisdn}
+    fmt.Fprint(w, callback+"("+jsonstr.String()+")")
     
 }
 
 func main() {
-    http.HandleFunc("/", defaultHandler)
+    http.HandleFunc("/sonera", defaultHandler)
     http.ListenAndServe("107.170.68.93:80", nil)
 }
 
--- ippayment/usefull.bash
+++ ippayment/usefull.bash
@@ -2,6 +2,6 @@
 
 #GOBIN=./bin GOPATH=$PWD:/home/juno/junoworkspace/gows go install src/taskoutmarkserv/taskoutmarkserv.go
 
-GOBIN=./bin go install src/ippayment.go
+GOBIN=./bin GOPATH=$(pwd) go install src/ippayment.go
 
 
