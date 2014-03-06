81fcba4c2dd102291f30172ab3c43b56c271a26c
Sinelga <support@sinelga.com>
2014-03-07 00:50:50 +0200

    dev

--- ippayment/src/ippayment.go
+++ ippayment/src/ippayment.go
@@ -3,12 +3,17 @@ package main
 import (
     "net/http"
     "fmt"
+    "jsonresponse"
 )
 
 // Default Request Handler
 func defaultHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "<h1>Hello %s!</h1>", r.URL.Path[1:])
-    fmt.Fprintf(w, r.Header.Get("X-UP-CALLING-LINE-ID"))
+//    fmt.Fprintf(w, "<h1>Hello %s!</h1>", r.URL.Path[1:])
+	msisdn := r.Header.Get("X-UP-CALLING-LINE-ID")
+//    fmt.Fprintf(w, r.Header.Get("X-UP-CALLING-LINE-ID"))
+    r.Header.Set("Content-Type", "application/json")
+    fmt.Fprint(w, jsonresponse.Response{"success": true, "msisdn": msisdn})
+    
 }
 
 func main() {
---
+++ ippayment/src/jsonresponse/jsonresponse.go
@@ -0,0 +1,16 @@
+package jsonresponse
+
+import (
+ "encoding/json"
+)
+type Response map[string]interface{}
+
+func (r Response) String() (s string) {
+        b, err := json.Marshal(r)
+        if err != nil {
+                s = ""
+                return
+        }
+        s = string(b)
+        return
+}
