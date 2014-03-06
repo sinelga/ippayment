4aab31085bbd20dfc74f3c790304459b51380dd2
Sinelga <support@sinelga.com>
2014-03-07 00:23:31 +0200

    dev

--- ippayment/src/ippayment.go
+++ ippayment/src/ippayment.go
@@ -8,10 +8,11 @@ import (
 // Default Request Handler
 func defaultHandler(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "<h1>Hello %s!</h1>", r.URL.Path[1:])
+    fmt.Fprintf(w, r.Header.Get("X-UP-CALLING-LINE-ID"))
 }
 
 func main() {
     http.HandleFunc("/", defaultHandler)
-    http.ListenAndServe(":31780", nil)
+    http.ListenAndServe(":80", nil)
 }
 
---
+++ ippayment/usefull.bash
@@ -0,0 +1,7 @@
+
+
+#GOBIN=./bin GOPATH=$PWD:/home/juno/junoworkspace/gows go install src/taskoutmarkserv/taskoutmarkserv.go
+
+GOBIN=./bin go install src/ippayment.go
+
+
