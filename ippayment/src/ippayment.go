8b302da3d45cedc2f2af461d446ff70fd47fd8e5
Sinelga <support@sinelga.com>
2014-03-08 04:36:31 +0200

    dev

--- ippayment/src/ippayment.go
+++ ippayment/src/ippayment.go
@@ -25,20 +25,27 @@ func (s FastCGIServer) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
 	msisdn := req.Header.Get("X-UP-CALLING-LINE-ID")
 
 	golog.Info("msisdn " + msisdn)
+	
+	callback := req.URL.Query().Get("callback")
+	//	fmt.Fprintf(w, "callback ",r.URL.RawQuery)
+	req.Header.Set("Content-Type", "application/json")
+	jsonstr := jsonresponse.Response{"success": true, "msisdn": msisdn}
+	fmt.Fprint(resp, callback+"("+jsonstr.String()+");")
+	
 
 }
 
 // Default Request Handler
-func defaultHandler(w http.ResponseWriter, r *http.Request) {
-	//    fmt.Fprintf(w, "<h1>Hello %s!</h1>", r.URL.Path[1:])
-	msisdn := r.Header.Get("X-UP-CALLING-LINE-ID")
-	callback := r.URL.Query().Get("callback")
-	//	fmt.Fprintf(w, "callback ",r.URL.RawQuery)
-	r.Header.Set("Content-Type", "application/json")
-	jsonstr := jsonresponse.Response{"success": true, "msisdn": msisdn}
-	fmt.Fprint(w, callback+"("+jsonstr.String()+");")
-
-}
+//func defaultHandler(w http.ResponseWriter, r *http.Request) {
+//	//    fmt.Fprintf(w, "<h1>Hello %s!</h1>", r.URL.Path[1:])
+//	msisdn := r.Header.Get("X-UP-CALLING-LINE-ID")
+//	callback := r.URL.Query().Get("callback")
+//	//	fmt.Fprintf(w, "callback ",r.URL.RawQuery)
+//	r.Header.Set("Content-Type", "application/json")
+//	jsonstr := jsonresponse.Response{"success": true, "msisdn": msisdn}
+//	fmt.Fprint(w, callback+"("+jsonstr.String()+");")
+//
+//}
 
 func main() {
 	//    http.HandleFunc("/sonera", defaultHandler)
