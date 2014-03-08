74c8e069e5628173b872683fe2d4206747b72787
Sinelga <support@sinelga.com>
2014-03-08 03:52:14 +0200

    dev

--- ippayment/src/ippayment.go
+++ ippayment/src/ippayment.go
@@ -1,26 +1,54 @@
-package main 
+package main
 
 import (
-    "net/http"
-    "fmt"
-    "jsonresponse"
-//    "net/url"
+	"fmt"
+	"jsonresponse"
+	"log"
+	"log/syslog"
+	"net"
+	"net/http"
+	"net/http/fcgi"
+	//    "net/url"
 )
+
+type FastCGIServer struct{}
+
+func (s FastCGIServer) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
+
+	golog, err := syslog.New(syslog.LOG_ERR, "golog")
+
+	defer golog.Close()
+	if err != nil {
+		log.Fatal("error writing syslog!!")
+	}
+
+	msisdn := req.Header.Get("X-UP-CALLING-LINE-ID")
+
+	golog.Info("msisdn " + msisdn)
+
+}
 
 // Default Request Handler
 func defaultHandler(w http.ResponseWriter, r *http.Request) {
-//    fmt.Fprintf(w, "<h1>Hello %s!</h1>", r.URL.Path[1:])
+	//    fmt.Fprintf(w, "<h1>Hello %s!</h1>", r.URL.Path[1:])
 	msisdn := r.Header.Get("X-UP-CALLING-LINE-ID")
 	callback := r.URL.Query().Get("callback")
-//	fmt.Fprintf(w, "callback ",r.URL.RawQuery)
-    r.Header.Set("Content-Type", "application/json")
-    jsonstr := jsonresponse.Response{"success": true, "msisdn": msisdn}
-    fmt.Fprint(w, callback+"("+jsonstr.String()+")")
-    
+	//	fmt.Fprintf(w, "callback ",r.URL.RawQuery)
+	r.Header.Set("Content-Type", "application/json")
+	jsonstr := jsonresponse.Response{"success": true, "msisdn": msisdn}
+	fmt.Fprint(w, callback+"("+jsonstr.String()+");")
+
 }
 
 func main() {
-    http.HandleFunc("/sonera", defaultHandler)
-    http.ListenAndServe("107.170.68.93:80", nil)
-}
+	//    http.HandleFunc("/sonera", defaultHandler)
+	//    http.ListenAndServe("107.170.68.93:80", nil)
 
+	listener, err := net.Listen("tcp", "127.0.0.1:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	srv := new(FastCGIServer)
+	fcgi.Serve(listener, srv)
+
+}
