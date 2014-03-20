46b80eb2ad2d4039b802c2c9e58493e78917bad6
Sinelga <support@sinelga.com>
2014-03-20 07:29:09 +0200

    prod

--- ippayment/src/logconverter.go
+++ ippayment/src/logconverter.go
@@ -1,4 +1,4 @@
-package main 
+package main
 
 import (
 	"bufio"
@@ -18,11 +18,9 @@ type Hit struct {
 	Provider string
 }
 
-
 func init() {
 	log.SetFlags(log.Lshortfile)
 }
-
 
 func main() {
 
@@ -30,44 +28,45 @@ func main() {
 	if err != nil {
 		log.Fatal("Error opening input file:", err)
 	}
- 
+
 	// Closes the file when we leave the scope of the current function,
 	// this makes sure we never forget to close the file if the
 	// function can exit in multiple places.
 	defer inputFile.Close()
- 
+
 	scanner := bufio.NewScanner(inputFile)
- 
+
 	// scanner.Scan() advances to the next token returning false if an error was encountered
-//	var records [][]string
-	var nextline bool=false
-	var msisdn string	 
+	//	var records [][]string
+	var nextline bool = false
+	var msisdn string
 	for scanner.Scan() {
 		line := scanner.Text()
-		
-		linesplit :=strings.Split(line," ")
-		
-//		fmt.Println(linesplit[5])
-		
-		if linesplit[5]=="msisdn" {
+
+		linesplit := strings.Split(line, " ")
+
+		//		fmt.Println(linesplit[5])
+
+		if linesplit[5] == "msisdn" {
 			nextline = true
-			msisdn = linesplit[6]			
+			msisdn = linesplit[6]
 		} else {
 			nextline = false
 		}
-		
+
 		if !nextline {
-			if linesplit[12] == "adult" {
-			outline := "curl -G -H \"X-UP-CALLING-LINE-ID: "+msisdn+"\" -d \"site="+linesplit[8]+"\" -d \"id="+linesplit[6]+"\" -d \"resource="+linesplit[10]+"\" -d \"themes=adult\" -d \"provider=sonera\" 127.0.0.1/sonera?callback=lslslslsl"
-//			fmt.Println("curl -G -H \"X-UP-CALLING-LINE-ID: \"",msisdn,linesplit[6],linesplit[8],linesplit[10],linesplit[12])
-			fmt.Println(outline)	
+			if len(linesplit) > 11 {
+				if linesplit[12] == "adult" {
+					outline := "curl -G -H \"X-UP-CALLING-LINE-ID: " + msisdn + "\" -d \"site=" + linesplit[8] + "\" -d \"id=" + linesplit[6] + "\" -d \"resource=" + linesplit[10] + "\" -d \"themes=adult\" -d \"provider=sonera\" 127.0.0.1/sonera?callback=lslslslsl"
+					//			fmt.Println("curl -G -H \"X-UP-CALLING-LINE-ID: \"",msisdn,linesplit[6],linesplit[8],linesplit[10],linesplit[12])
+					fmt.Println(outline)
+				}
+
 			}
-		
 		}
-				
-		
+
 	}
- 
+
 	// When finished scanning if any error other than io.EOF occured
 	// it will be returned by scanner.Err().
 	if err := scanner.Err(); err != nil {
@@ -75,4 +74,3 @@ func main() {
 	}
 
 }
-
