0f0b3cb55730c77954eb617b2ce7e51d988fdb78
Sinelga <support@sinelga.com>
2014-03-25 06:39:55 +0200

    prod

--- ippayment/src/elabsmsout/elabsmsout.go
+++ ippayment/src/elabsmsout/elabsmsout.go
@@ -2,26 +2,34 @@ package elabsmsout
 
 import (
 	"domains"
-	"time"
 	"log/syslog"
 	"strconv"
+	"time"
 )
 
-func Elab(golog syslog.Writer,clphonenum string,smsoutarr []domains.SmsOut) {
-	
+func Elab(golog syslog.Writer, clphonenum string, smsoutarr []domains.SmsOut) {
+
 	nowunix := time.Now().Unix()
-		
+
 	var lastdate int64
-	for _, smsout := range smsoutarr {
 
-		if lastdate < smsout.SmsCreated {
-			lastdate = smsout.SmsCreated
+	if len(smsoutarr) == 0 {
 
+		golog.Info("!!! not smsout for??? " + clphonenum)
+
+	} else {
+
+		for _, smsout := range smsoutarr {
+
+			if lastdate < smsout.SmsCreated {
+				lastdate = smsout.SmsCreated
+
+			}
 		}
+		difftime := (nowunix - lastdate)
+
+		golog.Info("Time sends SMS min? last " + clphonenum + " " + strconv.FormatInt(lastdate, 10) + " diff " + strconv.FormatInt(difftime, 10))
+
 	}
-	difftime := (nowunix - lastdate)
-	
-	golog.Info("Time sends SMS min? last "+clphonenum +" "+ strconv.FormatInt(lastdate,10)+" diff "+ strconv.FormatInt(difftime,10))
-		
 
 }
