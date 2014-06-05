53131dcfe64d7cb83d9e83f908c70a3bf18613b5
juno <juno@krasava.fi>
2014-06-05 09:11:34 +0300

    prod

--- ippayment/crossdeployment.bash
+++ ippayment/crossdeployment.bash
@@ -12,13 +12,13 @@ rm bin/tstip
 
 git pull
 
-GOBIN=./bin GOPATH=$(pwd) go install src/ippayment.go
-GOBIN=./bin GOPATH=$(pwd) go install src/hitshandler.go
-GOBIN=./bin GOPATH=$(pwd) go install src/allcollection.go
-GOBIN=./bin GOPATH=$(pwd) go install src/showcollection.go
-GOBIN=./bin GOPATH=$(pwd) go install src/start.go
-GOBIN=./bin GOPATH=$(pwd) go install src/logconverter.go
-GOBIN=./bin GOPATH=$(pwd) go install src/sendsms.go
-GOBIN=./bin GOPATH=$(pwd) go install src/tstip.go
+GOBIN=./bin GOPATH=$(pwd):/home/juno/workspace/gocode go install src/ippayment.go
+GOBIN=./bin GOPATH=$(pwd:/home/juno/workspace/gocode) go install src/hitshandler.go
+GOBIN=./bin GOPATH=$(pwd):/home/juno/workspace/gocode go install src/allcollection.go
+GOBIN=./bin GOPATH=$(pwd):/home/juno/workspace/gocode go install src/showcollection.go
+GOBIN=./bin GOPATH=$(pwd):/home/juno/workspace/gocode go install src/start.go
+GOBIN=./bin GOPATH=$(pwd):/home/juno/workspace/gocode go install src/logconverter.go
+GOBIN=./bin GOPATH=$(pwd):/home/juno/workspace/gocode go install src/sendsms.go
+GOBIN=./bin GOPATH=$(pwd):/home/juno/workspace/gocode go install src/tstip.go
 
 
